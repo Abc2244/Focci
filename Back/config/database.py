1bab750ada2faaf0695169fa4ed6839abe6c35f4
Abc2244 <bjulian127@gmail.com>
2025-02-10 12:18:45 -0500

    Estructure for api organize and test passed

--- Back/config/database.py
+++ Back/config/database.py
@@ -4,9 +4,6 @@ from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv(dotenv_path="/Users/juliveevart/Documents/University/Focci/Back/.env")
-print("MONGO_URI =", os.getenv("MONGO_URI"))
-print("MONGO_DB  =", os.getenv("MONGO_DB"))
-print("SECRET_KEY =", os.getenv("SECRET_KEY"))
 
 class MongoDB:
     def __init__(self):
--- Back/main.py
+++ Back/main.py
@@ -1,22 +1,26 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.routes import router # Asegúrate de importar el archivo donde están tus rutas
+from api.routes import auth_router, users_router, subjects_router, tasks_router, reminders_router
 
 app = FastAPI()
 
 # Habilitar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Permitir cualquier origen (útil para desarrollo). Puedes restringirlo en producción.
+    allow_origins=["*"],        # Permitir cualquier origen (ajusta en producción)
     allow_credentials=True,
-    allow_methods=["*"],  # Permitir todos los métodos HTTP (GET, POST, PUT, DELETE, etc.)
-    allow_headers=["*"],  # Permitir todos los encabezados
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Registrar las rutas que has definido en el archivo de rutas
-app.include_router(router)
+# Incluir cada router
+app.include_router(auth_router)
+app.include_router(users_router)
+app.include_router(subjects_router)
+app.include_router(tasks_router)
+app.include_router(reminders_router)
 
-# Opcional: puedes agregar una ruta de prueba para verificar que la API esté corriendo
+# Ruta de prueba
 @app.get("/")
 async def root():
     return {"message": "API is running!"}
