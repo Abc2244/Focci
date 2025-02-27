414ee2fa535f5e1e64a0fed1d70d73c1123a5e53
Abc2244 <bjulian127@gmail.com>
2025-02-26 19:59:56 -0500

    acomodando reminders

--- Back/api/routes/reminders.py
+++ Back/api/routes/reminders.py
@@ -35,10 +35,16 @@ async def delete_reminder(reminder_id: str):
 
 @router.get("/users/{user_id}/reminders/upcoming/")
 async def get_upcoming_reminders(user_id: str):
+    print(f"Buscando recordatorios para el usuario: {user_id}")
+    current_time = datetime.datetime.utcnow()
+    print(f"Fecha actual: {current_time}")
+    
     reminders = await mongodb.get_collection("reminders").find({
         "user_id": user_id,
-        "reminder_date": {"$gte": datetime.datetime.utcnow()}
+        "reminder_date": {"$gte": current_time}
     }).to_list(length=100)
+    
+    print(f"Recordatorios encontrados: {len(reminders)}")
     return [serialize_mongo_document(reminder) for reminder in reminders]
 
 @router.get("/users/{user_id}/reminders/priority/{level}/")
